39a20d2d023101314eeb17b978af5a7d62a83d1e
Oliver Lowe <o@olowe.co>
2024-07-09 19:41:50 +1000

    delete unused build script

--- bin/browser-shim.js
+++
@@ -1,9 +0,0 @@
-// browser shims that run in node, so that page-lifecycle won't error
-global.self = global
-global.document = {
-  visibilityState: 'visible',
-  hasFocus: () => true,
-  wasDiscarded: false
-}
-global.addEventListener = () => {}
-global.removeEventListener = () => {}
