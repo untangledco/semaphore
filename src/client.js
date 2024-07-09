af69c15b508ee22ad010bdf9d2244b89dabd9abc
Oliver Lowe <o@olowe.co>
2024-07-09 14:08:43 +1000

    Remove safari-idb-14
    This was fixed in Safari 14.7. The current version of Safari is 17.

--- src/client.js
+++ src/client.js
@@ -6,9 +6,8 @@ import './routes/_utils/forceOnline.js'
 import { mark, stop } from './routes/_utils/marks.js'
 import { loadPolyfills } from './routes/_utils/polyfills/loadPolyfills.js'
 import { loadNonCriticalPolyfills } from './routes/_utils/polyfills/loadNonCriticalPolyfills.js'
-import idbReady from 'safari-14-idb-fix/dist/esm'
 
-Promise.all([idbReady(), loadPolyfills()]).then(() => {
+loadPolyfills().then(() => {
   mark('sapperStart')
   sapper.start({ target: document.querySelector('#sapper') })
   stop('sapperStart')
