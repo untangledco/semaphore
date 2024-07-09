fa80e402333a6e5b1658356fb122da736798cbc0
Oliver Lowe <o@olowe.co>
2024-07-09 18:43:16 +1000

    blurhash: drop pre-chrome 82 optimisation

--- src/routes/_workers/blurhash.js
+++ src/routes/_workers/blurhash.js
@@ -1,11 +1,8 @@
 import { decode as decodeBlurHash } from 'blurhash'
 import registerPromiseWorker from 'promise-worker/register.js'
 import { BLURHASH_RESOLUTION as RESOLUTION } from '../_static/blurhash.js'
-import { isChromePre82 } from '../_utils/userAgent/isChromePre82.js'
 
-// Disabled in Chrome because convertToBlob() is slow
-// https://github.com/nolanlawson/pinafore/issues/1396
-const OFFSCREEN_CANVAS = !isChromePre82() && typeof OffscreenCanvas === 'function'
+const OFFSCREEN_CANVAS = typeof OffscreenCanvas === 'function'
   ? new OffscreenCanvas(RESOLUTION, RESOLUTION)
   : null
 const OFFSCREEN_CANVAS_CONTEXT_2D = OFFSCREEN_CANVAS
