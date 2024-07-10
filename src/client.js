602f49fa7c44fa557ae27ece9c76f1131088c1b0
Oliver Lowe <o@olowe.co>
2024-07-10 13:11:22 +1000

    client: stop measuring performance of... loading.. something?
    The browser - the runtime - gives us enough info, jesus!

--- src/client.js
+++ src/client.js
@@ -2,12 +2,8 @@ import * as sapper from '../__sapper__/client.js'
 import './routes/_utils/serviceWorkerClient.js'
 import './routes/_utils/historyEvents.js'
 import './routes/_utils/forceOnline.js'
-import { mark, stop } from './routes/_utils/marks.js'
 
-mark('sapperStart')
 sapper.start({ target: document.querySelector('#sapper') })
-stop('sapperStart')
-
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept()
 }
