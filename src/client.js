a9e965c9ebfc177ffa7374eadb44ccd0c6316c8d
Oliver Lowe <o@olowe.co>
2024-07-10 13:08:49 +1000

    routes/_utils: remove more polyfills
    What is Chesterton's Fence? But really, what is this stuff?
    While here remove obsolete theme docs.

--- src/client.js
+++ src/client.js
@@ -3,15 +3,10 @@ import './routes/_utils/serviceWorkerClient.js'
 import './routes/_utils/historyEvents.js'
 import './routes/_utils/forceOnline.js'
 import { mark, stop } from './routes/_utils/marks.js'
-import { loadPolyfills } from './routes/_utils/polyfills/loadPolyfills.js'
 
-loadPolyfills().then(() => {
-  mark('sapperStart')
-  sapper.start({ target: document.querySelector('#sapper') })
-  stop('sapperStart')
-})
-
-console.log('process.env.NODE_ENV', process.env.NODE_ENV)
+mark('sapperStart')
+sapper.start({ target: document.querySelector('#sapper') })
+stop('sapperStart')
 
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept()
