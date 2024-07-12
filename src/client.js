8a4cfa3051cf119ea8d6c9c68607b5c317c06fb0
Oliver Lowe <o@olowe.co>
2024-07-12 14:14:41 +1000

    Del obsolete integration test workaround

--- src/client.js
+++ src/client.js
@@ -1,7 +1,6 @@
 import * as sapper from '../__sapper__/client.js'
 import './routes/_utils/serviceWorkerClient.js'
 import './routes/_utils/historyEvents.js'
-import './routes/_utils/forceOnline.js'
 
 sapper.start({ target: document.querySelector('#sapper') })
 if (import.meta.webpackHot) {
